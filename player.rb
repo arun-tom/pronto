ad18aac0706997b59956ca97015292dc1f2d0025
arun-tom <thomasaruntom@gmail.com>
2022-07-24 04:35:29 +1000

    Added dice rolling option everytim  with press of a a key

--- player.rb
+++ player.rb
@@ -1,10 +1,11 @@
 class Player
-    attr_accessor :name, :money, :play_order, :health
+    attr_accessor :name, :money, :play_order, :location, :health
 
     def initialize(name, play_order)
         @name = name
         @money = 16
         @play_order = play_order
         @health = 100
+        @location = 0
     end
 end
--- pronto.rb
+++ pronto.rb
@@ -1,11 +1,16 @@
 require 'json'
 require './board.rb'
 require './player.rb'
-require './dice.rb'
 
 
 file = File.read('board.json')
 board_data = JSON.parse(file)
+file_1 = File.read('rolls_1.json')
+$rolls_1_data = JSON.parse(file_1)
+file_2 = File.read('rolls_2.json')
+$rolls_2_data = JSON.parse(file_2)
+$dice_1_index = 0
+$dice_2_index = 0
 
 puts "Hello, Pronto!"
 
@@ -55,5 +60,51 @@ puts peter.play_order
 puts peter.money
 puts peter.health
 puts "\n"
+
+#dice = Dice.new
+
+
+
+puts $rolls_1_data[1]
+puts $rolls_1_data[2]
+puts $rolls_2_data[1]
+puts $rolls_2_data[2]
+
+
+def rollDice
+# Go through rolls_1.json and rolls_2.json files and returns sum of corresponding elements in each files in every pass.
+    rolled_sum = $rolls_1_data[$dice_1_index] + $rolls_2_data[$dice_2_index]
+        puts $rolls_1_data[$dice_1_index]
+        puts $rolls_2_data[$dice_2_index]
+        #puts rolled_sum
+
+    $dice_1_index += 1
+    $dice_2_index += 1
+
+    if $dice_1_index >= $rolls_1_data.length
+        $dice_1_index = 0
+    end
+
+    if $dice_2_index >= $rolls_2_data.length
+        $dice_2_index = 0
+    end
+    return rolled_sum
+end
+
+# Infinite loop to continue play the game until operator wishes to exit
+while 1
+    puts rollDice
+    puts $dice_1_index
+    puts $dice_2_index
+    puts "Press Enter to continue or 'q' to exit"
+    string = (gets.chomp().to_s).downcase
+
+    if string == "q"
+        break
+    end
+end
+
+
+
 
 
